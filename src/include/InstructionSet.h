f3b6b93647fdbc2aa76b525542bf2a4c8ea69eba
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-10-31 10:28:04 -0400

    Added abstract class and initial intruction set for control instructions

---
+++ src/include/InstructionSet.h
@@ -0,0 +1,9 @@
+#ifndef INSTRUCTION_SET_H
+#define INSTRUCTION_SET_H
+#include <cstdint>
+#include "cpu.h"
+class InstructionSet {
+public:
+    virtual bool execute(uint8_t opcode, CPU& cpu) = 0;
+};
+#endif // INSTRUCTION_SET_H
