23790ea009f4d856ed61e731c9f96fa11d49a0cf
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-10-31 08:56:17 -0400

    Added a header file to represent macros in ControlSet

---
+++ src/include/ControlInstructionSet.h
@@ -0,0 +1,11 @@
+#ifndef CONTROL_INSTRUCTION_SET_H
+#define CONTROL_INSTRUCTION_SET_H
+
+#define NOP 0x00
+#define HALT 0x76
+#define STOP 0x10
+#define PREFIX_CB 0xCB
+#define DI 0xF3
+#define EI 0xFB
+
+#endif // CONTROL_INSTRUCTION_SET_H
