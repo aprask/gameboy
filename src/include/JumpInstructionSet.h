09c8bb2c49319cde8bf3b099fde5d3bb0752357f
Jakob Olsen <jolsen16@charlotte.edu>
2024-11-02 01:15:01 -0400

    Fixed naming convention error with RST_NN instructions

--- src/include/JumpInstructionSet.h
+++ src/include/JumpInstructionSet.h
@@ -13,27 +13,27 @@
 #define JP_NZ_a16 0xC2
 #define JP_a16 0xC3
 #define CALL_NZ_a16 0xC4
-#define RST_00 0xC7
+#define RST_00H 0xC7
 #define RET_Z 0xC8
 #define RET 0xC9
 #define JP_Z_a16 0xCA
 #define CALL_Z_a16 0xCC
 #define CALL_a16 0xCD
-#define RST_08 0xCF
+#define RST_08H 0xCF
 #define RET_NC 0xD0
 #define JP_NC_a16 0xD2
 #define CALL_NC_a16 0xD4
-#define RST_10 0xD7
+#define RST_10H 0xD7
 #define RET_C 0xD8
 #define RETI 0xD9
 #define JP_C_a16 0xDA
 #define CALL_C_a16 0xDC
-#define RST_18 0xDF
-#define RST_20 0xE7
+#define RST_18H 0xDF
+#define RST_20H 0xE7
 #define JP_HL 0xE9
-#define RST_28 0xEF
-#define RST_30 0xF7
-#define RST_38 0xFF
+#define RST_28H 0xEF
+#define RST_30H 0xF7
+#define RST_38H 0xFF
 
 class JumpInstructionSet : public InstructionSet {
 public:
@@ -49,27 +49,27 @@ private:
     void jp_nz_a16(CPU& cpu);
     void jp_a16(CPU& cpu);
     void call_nz_a16(CPU& cpu);
-    void rst_00(CPU& cpu);
+    void rst_00H(CPU& cpu);
     void ret_z(CPU& cpu);
     void ret(CPU& cpu);
     void jp_z_a16(CPU& cpu);
     void call_z_a16(CPU& cpu);
     void call_a16(CPU& cpu);
-    void rst_08(CPU& cpu);
+    void rst_08H(CPU& cpu);
     void ret_nc(CPU& cpu);
     void jp_nc_a16(CPU& cpu);
     void call_nc_a16(CPU& cpu);
-    void rst_10(CPU& cpu);
+    void rst_10H(CPU& cpu);
     void ret_c(CPU& cpu);
     void reti(CPU& cpu);
     void jp_c_a16(CPU& cpu);
     void call_c_a16(CPU& cpu);
-    void rst_18(CPU& cpu);
-    void rst_20(CPU& cpu);
+    void rst_18H(CPU& cpu);
+    void rst_20H(CPU& cpu);
     void jp_hl(CPU& cpu);
-    void rst_28(CPU& cpu);
-    void rst_30(CPU& cpu);
-    void rst_38(CPU& cpu);
+    void rst_28H(CPU& cpu);
+    void rst_30H(CPU& cpu);
+    void rst_38H(CPU& cpu);
 };
 
 #endif // JUMP_INSTRUCTION_SET_H
