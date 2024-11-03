c9ae264410be5bcb15be788a9c4d44398a850e8a
Andrew Terrence Praskala <andrewterrenceprask@outlook.com>
2024-11-03 08:27:54 -0500

    Created function declarations for executing functions associated w/ memory locations pre-defined

--- src/include/16bitLoadInstructionSet.h
+++ src/include/16bitLoadInstructionSet.h
@@ -2,7 +2,6 @@
 #include "cpu.h"
 #ifndef SIXTEEN_BIT_LOAD_INSTRUCTION_SET_H
 #define SIXTEEN_BIT_LOAD_INSTRUCTION_SET_H
-// #define LD_BC_A 0x02
 #define LD_BC_n16 0x01
 #define LD_a16_SP 0x08
 #define LD_DE_n16 0x11
@@ -18,4 +17,25 @@
 #define PUSH_AF 0xF5
 #define LP_HL_SP_e8 0xF8
 #define LP_SP_HL 0xF9
+
+class SixteenBitLoadInstructionSet : public InstructionSet {
+    public:
+        bool execute(uint8_t opcode, CPU& cpu) override;
+    private:
+        void ld_bc_n16(CPU& cpu);
+        void ld_a16_sp(CPU& cpu);
+        void ld_de_n16(CPU& cpu);
+        void ld_hl_n16(CPU& cpu);
+        void ld_sp_n16(CPU& cpu);
+        void pop_bc(CPU& cpu);
+        void pop_de(CPU& cpu);
+        void pop_hl(CPU& cpu);
+        void pop_af(CPU& cpu);
+        void push_bc(CPU& cpu);
+        void push_de(CPU& cpu);
+        void push_hl(CPU& cpu);
+        void push_af(CPU& cpu);
+        void lp_hl_sp_e8(CPU& cpu);
+        void lp_sp_hl(CPU& cpu);
+};
 #endif // SIXTEEN_BIT_LOAD_INSTRUCTION_SET_H
