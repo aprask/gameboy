e8e3285295622a6335ad3bd00b16d310623b9228
Jakob Olsen <jolsen618@gmail.com>
2024-11-08 23:03:31 -0500

    Added prototypes for generic functions
    I have set the Lord always before me: because he is at my right hand, I shall not be moved. Psalm 16:8 KJV

--- src/include/8bitShiftInstructionSet.h
+++ src/include/8bitShiftInstructionSet.h
@@ -271,6 +271,28 @@ public:
 
 private:
     void rlca(CPU& cpu);
+    void rlc_r(CPU& cpu, Byte& reg);
+    void rlc_hl_indirect(CPU& cpu);
+    void rrc_r(CPU& cpu, Byte& reg);
+    void rrc_hl_indirect(CPU& cpu);
+    void rl_r(CPU& cpu, Byte& reg);
+    void rl_hl_indirect(CPU& cpu);
+    void rr_r(CPU& cpu, Byte& reg);
+    void rr_hl_indirect(CPU& cpu);
+    void sla_r(CPU& cpu, Byte& reg);
+    void sla_hl_indirect(CPU& cpu);
+    void sra_r(CPU& cpu, Byte& reg);
+    void sra_hl_indirect(CPU& cpu);
+    void swap_r(CPU& cpu, Byte& reg);
+    void swap_hl_indirect(CPU& cpu);
+    void srl_r(CPU& cpu, Byte& reg);
+    void srl_hl_indirect(CPU& cpu);
+    void bit_n_r(uint8_t bit, CPU& cpu, Byte& reg);
+    void bit_n_hl_indirect(uint8_t bit, CPU& cpu);
+    void res_n_r(uint8_t bit, CPU& cpu, Byte& reg);
+    void res_n_hl_indirect(uint8_t bit, CPU& cpu);
+    void set_n_r(uint8_t bit, CPU& cpu, Byte& reg);
+    void set_n_hl_indirect(uint8_t bit, CPU& cpu);
     void rlc_a(CPU& cpu);
     void rlc_b(CPU& cpu);
     void rlc_c(CPU& cpu);
