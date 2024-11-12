a5a25985b41303e1e935e789667f068c07bcbd8b
Jakob Olsen <jolsen618@gmail.com>
2024-11-11 23:30:10 -0500

    Added several functions to CPU registers struct, added function skeleton to prepare for use of mapping
    Major overhaul of current framework imminent, including every function, class and switch statement.

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -16,3 +16,13 @@ void CPU::write(Word address, Byte data) {
 Byte CPU::read(Word address) {
     return bus.read(address);
 }
+
+void CPU::initializeInstructionTable() {
+    // This is where we are going to have each InstructionSet class add their own instructions to the table
+    // The goal is modularity, this will be called on startup... not much overhead once the table is initialized... I hope.
+}
+
+bool CPU::executeInstruction(Word opcode) {
+    // This is where we will look up the opcode in the table and execute the instruction
+    return false;
+}
--- src/include/cpu.h
+++ src/include/cpu.h
@@ -3,8 +3,16 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <unordered_map>
+#include <functional>
 #include "bus.h"
 
+#define Z 7;
+#define N 6;
+#define H 5;
+#define C 4;
+
+// TO BE REMOVED -- THIS IS SO EVERYTHING CAN STILL WORK
 #define FLAG_ZERO 0b10000000
 #define FLAG_SUBTRACT 0b01000000
 #define FLAG_HALF_CARRY 0b00100000
@@ -12,6 +20,13 @@
 
 using Byte = uint8_t; // 8 bits
 using Word = uint16_t; // 16 bits
+using InstructionHandler = std::function<void(InstructionContext)>;
+
+typedef struct {
+    CPU* cpu;
+    Byte* reg;
+    bool bit;
+} InstructionContext;
 
 typedef struct {
     Word program_counter;
@@ -20,10 +35,10 @@ typedef struct {
     // Special purpose registers
     Byte instruction_register;
     Byte interrupt_enable;
+    Byte a_register; // Accumulator
+    Byte flag_register; // Four least-significant bits will ALWAYS be zero.
 
     // General purpose registers
-    Byte a_register;
-    Byte flag_register; // Four least-significant bits will ALWAYS be zero.
     Byte b_register;
     Byte c_register;
     Byte d_register;
@@ -31,12 +46,34 @@ typedef struct {
     Byte h_register; // High register
     Byte l_register; // Low register
 
+    // TO BE REMOVED -- THIS IS SO EVERYTHING CAN STILL WORK
     void set_flag(Byte flag, bool value) {
         if (value) {
-            flag_register |= flag;
+            set_bit(flag_register, flag);
         } else {
-            flag_register &= ~flag;
+            res_bit(flag_register, flag);
         }
+    }
+
+    Word get_pair(Byte high, Byte low) {
+        return (high << 8) | low;
+    }
+
+    void set_pair(Word value, Byte& high, Byte& low) {
+        high = (value & 0xFF00) >> 8;
+        low = value & 0x00FF;
+    }
+
+    bool get_bit(Byte reg, Byte bit) {
+        return (reg >> bit) & 1;
+    }
+
+    void set_bit(Byte& reg, Byte bit) {
+        reg |= (1 << bit);
+    }
+
+    void res_bit(Byte& reg, Byte bit) {
+        reg &= ~(1 << bit);
     }
 } Registers;
 
@@ -50,6 +87,12 @@ public:
     void cycle();
     void write(Word address, Byte data);
     Byte read(Word address);
+
+    void initializeInstructionTable();
+    bool executeInstruction(Word opcode);
+
+private:
+    std::unordered_map<Word, InstructionHandler> instructionTable;
 };
 
 #endif // CPU_H
