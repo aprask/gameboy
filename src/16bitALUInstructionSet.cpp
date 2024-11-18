83fd0b72fbeec847f99e432d27d5295daff1919f
Jakob Olsen <jolsen618@gmail.com>
2024-11-18 01:21:55 -0500

    Refactored 16bitALU and 16bitLoad

--- src/16bitALUInstructionSet.cpp
+++ src/16bitALUInstructionSet.cpp
@@ -1,56 +1,44 @@
 #include "include/16bitALUInstructionSet.h"
 
 void SixteenBitALUInstructionSet::initializeInstructionTable(CPU& cpu) {
-    cpu.addInstruction(0x03, [this, &cpu] { this->inc_rr(cpu, cpu.registers.b_register, cpu.registers.c_register); });
-    cpu.addInstruction(0x09, [this, &cpu] { this->add_hl_rr(cpu, cpu.registers.b_register, cpu.registers.c_register); });
-    cpu.addInstruction(0x0B, [this, &cpu] { this->dec_rr(cpu, cpu.registers.b_register, cpu.registers.c_register); });
-    cpu.addInstruction(0x13, [this, &cpu] { this->inc_rr(cpu, cpu.registers.d_register, cpu.registers.e_register); });
-    cpu.addInstruction(0x19, [this, &cpu] { this->add_hl_rr(cpu, cpu.registers.d_register, cpu.registers.e_register); });
-    cpu.addInstruction(0x1B, [this, &cpu] { this->dec_rr(cpu, cpu.registers.d_register, cpu.registers.e_register); });
-    cpu.addInstruction(0x23, [this, &cpu] { this->inc_rr(cpu, cpu.registers.h_register, cpu.registers.l_register); });
-    cpu.addInstruction(0x29, [this, &cpu] { this->add_hl_rr(cpu, cpu.registers.h_register, cpu.registers.l_register); });
-    cpu.addInstruction(0x2B, [this, &cpu] { this->dec_rr(cpu, cpu.registers.h_register, cpu.registers.l_register); });
+    cpu.addInstruction(0x03, [this, &cpu] { this->inc_rr(cpu, BC); });
+    cpu.addInstruction(0x09, [this, &cpu] { this->add_hl_rr(cpu, BC); });
+    cpu.addInstruction(0x0B, [this, &cpu] { this->dec_rr(cpu, BC); });
+    cpu.addInstruction(0x13, [this, &cpu] { this->inc_rr(cpu, DE); });
+    cpu.addInstruction(0x19, [this, &cpu] { this->add_hl_rr(cpu, DE); });
+    cpu.addInstruction(0x1B, [this, &cpu] { this->dec_rr(cpu, DE); });
+    cpu.addInstruction(0x23, [this, &cpu] { this->inc_rr(cpu, HL); });
+    cpu.addInstruction(0x29, [this, &cpu] { this->add_hl_rr(cpu, HL); });
+    cpu.addInstruction(0x2B, [this, &cpu] { this->dec_rr(cpu, HL); });
     cpu.addInstruction(0x33, [this, &cpu] { this->inc_sp(cpu); });
     cpu.addInstruction(0x39, [this, &cpu] { this->add_hl_sp(cpu); });
     cpu.addInstruction(0x3B, [this, &cpu] { this->dec_sp(cpu); });
     cpu.addInstruction(0xE8, [this, &cpu] { this->add_sp_e8(cpu); });
 }
 
-void SixteenBitALUInstructionSet::inc_rr(CPU& cpu, Byte& reg1, Byte& reg2) {
-    Word reg = reg1 << 8 | reg2;
+void SixteenBitALUInstructionSet::inc_rr(CPU& cpu, int pair) {
+    Word reg = cpu.getPair(pair);
     reg++;
-    reg2 = reg & 0xFF; // Lower byte
-    reg1 = reg >> 8; // Upper byte
+    cpu.setPair(pair, reg);
 }
 
-void SixteenBitALUInstructionSet::dec_rr(CPU& cpu, Byte& reg1, Byte& reg2) {
-    Word reg = reg1 << 8 | reg2;
+void SixteenBitALUInstructionSet::dec_rr(CPU& cpu, int pair) {
+    Word reg = cpu.getPair(pair);
     reg--;
-    reg2 = reg & 0xFF;
-    reg1 = reg >> 8;
+    cpu.setPair(pair, reg);
 }
 
-void SixteenBitALUInstructionSet::add_hl_rr(CPU& cpu, Byte& reg1, Byte& reg2) {
-    Word hl = cpu.registers.h_register << 8 | cpu.registers.l_register;
-    Word reg = reg1 << 8 | reg2;
+void SixteenBitALUInstructionSet::add_hl_rr(CPU& cpu, int pair) {
+    Word hl = cpu.getPair(HL);
+    Word reg = cpu.getPair(pair);
     Word result = hl + reg;
 
-    cpu.registers.l_register = result & 0xFF;
-    cpu.registers.h_register = result >> 8;
+    cpu.setPair(HL, result);
 
-    cpu.registers.set_flag(FLAG_SUBTRACT, false);
-
-    if (result & 0x8000 == 0) {
-        cpu.registers.set_flag(FLAG_CARRY, false);
-    } else {
-        cpu.registers.set_flag(FLAG_CARRY, true);
-    }
-
-    if (result & 0x0080 == 0) {
-        cpu.registers.set_flag(FLAG_HALF_CARRY, false);
-    } else {
-        cpu.registers.set_flag(FLAG_HALF_CARRY, true);
-    }
+    cpu.setBit(cpu.registers.f, Z, false);
+    cpu.setBit(cpu.registers.f, N, false);
+    cpu.setBit(cpu.registers.f, H, (result & 0x0080) == 1);
+    cpu.setBit(cpu.registers.f, C, (result & 0x8000) == 1);
 }
 
 void SixteenBitALUInstructionSet::inc_sp(CPU& cpu) {
@@ -58,10 +46,9 @@ void SixteenBitALUInstructionSet::inc_sp(CPU& cpu) {
 }
 
 void SixteenBitALUInstructionSet::add_hl_sp(CPU& cpu) {
-    Word hl = cpu.registers.h_register << 8 | cpu.registers.l_register;
+    Word hl = cpu.getPair(HL);
     hl += cpu.registers.stack_pointer;
-    cpu.registers.l_register = hl & 0xFF;
-    cpu.registers.h_register = hl >> 8;
+    cpu.setPair(HL, hl);
 }
 
 void SixteenBitALUInstructionSet::dec_sp(CPU& cpu) {
@@ -69,24 +56,13 @@ void SixteenBitALUInstructionSet::dec_sp(CPU& cpu) {
 }
 
 void SixteenBitALUInstructionSet::add_sp_e8(CPU& cpu) {
-    int8_t data = (int8_t) cpu.bus.read(cpu.registers.program_counter); // The SIGNED immediate value is stored in the next byte
-    cpu.registers.program_counter++; // Increment program counter
+    int8_t data = (int8_t) cpu.fetchPC(); // The SIGNED immediate value is stored in the next byte
 
     Word result = cpu.registers.stack_pointer + data;
     cpu.registers.stack_pointer = result;
 
-    cpu.registers.set_flag(FLAG_ZERO, false);
-    cpu.registers.set_flag(FLAG_SUBTRACT, false);
-
-    if (result & 0x8000 == 0) {
-        cpu.registers.set_flag(FLAG_CARRY, false);
-    } else {
-        cpu.registers.set_flag(FLAG_CARRY, true);
-    }
-
-    if (result & 0x0080 == 0) {
-        cpu.registers.set_flag(FLAG_HALF_CARRY, false);
-    } else {
-        cpu.registers.set_flag(FLAG_HALF_CARRY, true);
-    }
+    cpu.setBit(cpu.registers.f, Z, false);
+    cpu.setBit(cpu.registers.f, N, false);
+    cpu.setBit(cpu.registers.f, H, (result & 0x0080) == 1);
+    cpu.setBit(cpu.registers.f, C, (result & 0x8000) == 1);
 }
--- src/16bitLoadInstructionSet.cpp
+++ src/16bitLoadInstructionSet.cpp
@@ -1,36 +1,34 @@
 #include "include/16bitLoadInstructionSet.h"
 
 void SixteenBitLoadInstructionSet::initializeInstructionTable(CPU& cpu) {
-    cpu.addInstruction(0x01, [this, &cpu] { this->ld_rr_n16(cpu, cpu.registers.b_register, cpu.registers.c_register); });
+    cpu.addInstruction(0x01, [this, &cpu] { this->ld_rr_n16(cpu, BC); });
     cpu.addInstruction(0x08, [this, &cpu] { this->ld_a16_sp(cpu); });
-    cpu.addInstruction(0x11, [this, &cpu] { this->ld_rr_n16(cpu, cpu.registers.d_register, cpu.registers.e_register); });
-    cpu.addInstruction(0x21, [this, &cpu] { this->ld_rr_n16(cpu, cpu.registers.h_register, cpu.registers.l_register); });
+    cpu.addInstruction(0x11, [this, &cpu] { this->ld_rr_n16(cpu, DE); });
+    cpu.addInstruction(0x21, [this, &cpu] { this->ld_rr_n16(cpu, HL); });
     cpu.addInstruction(0x31, [this, &cpu] { this->ld_sp_n16(cpu); });
-    cpu.addInstruction(0xC1, [this, &cpu] { this->pop_rr(cpu, cpu.registers.b_register, cpu.registers.c_register); });
-    cpu.addInstruction(0xD1, [this, &cpu] { this->pop_rr(cpu, cpu.registers.d_register, cpu.registers.e_register); });
-    cpu.addInstruction(0xE1, [this, &cpu] { this->pop_rr(cpu, cpu.registers.h_register, cpu.registers.l_register); });
-    cpu.addInstruction(0xF1, [this, &cpu] { this->pop_rr(cpu, cpu.registers.a_register, cpu.registers.flag_register); });
-    cpu.addInstruction(0xC5, [this, &cpu] { this->push_rr(cpu, cpu.registers.b_register, cpu.registers.c_register); });
-    cpu.addInstruction(0xD5, [this, &cpu] { this->push_rr(cpu, cpu.registers.d_register, cpu.registers.e_register); });
-    cpu.addInstruction(0xE5, [this, &cpu] { this->push_rr(cpu, cpu.registers.h_register, cpu.registers.l_register); });
-    cpu.addInstruction(0xF5, [this, &cpu] { this->push_rr(cpu, cpu.registers.a_register, cpu.registers.flag_register); });
+    cpu.addInstruction(0xC1, [this, &cpu] { this->pop_rr(cpu, BC); });
+    cpu.addInstruction(0xD1, [this, &cpu] { this->pop_rr(cpu, DE); });
+    cpu.addInstruction(0xE1, [this, &cpu] { this->pop_rr(cpu, HL); });
+    cpu.addInstruction(0xF1, [this, &cpu] { this->pop_rr(cpu, AF); });
+    cpu.addInstruction(0xC5, [this, &cpu] { this->push_rr(cpu, BC); });
+    cpu.addInstruction(0xD5, [this, &cpu] { this->push_rr(cpu, DE); });
+    cpu.addInstruction(0xE5, [this, &cpu] { this->push_rr(cpu, HL); });
+    cpu.addInstruction(0xF5, [this, &cpu] { this->push_rr(cpu, AF); });
     cpu.addInstruction(0xF8, [this, &cpu] { this->lp_hl_sp_e8(cpu); });
     cpu.addInstruction(0xF9, [this, &cpu] { this->lp_sp_hl(cpu); });
 }
 
-void SixteenBitLoadInstructionSet::ld_rr_n16(CPU& cpu, Byte& high, Byte& low) {
-    low = cpu.read(cpu.registers.program_counter);
-    cpu.registers.program_counter++;
-    high = cpu.read(cpu.registers.program_counter);
-    cpu.registers.program_counter++;
+void SixteenBitLoadInstructionSet::ld_rr_n16(CPU& cpu, int pair) {
+    Byte low = cpu.read(cpu.fetchPC());
+    Byte high = cpu.read(cpu.fetchPC());
+
+    cpu.setPair(pair, high << 8 | low);
 }
 
 
 void SixteenBitLoadInstructionSet::ld_a16_sp(CPU& cpu) {
-    Byte low = cpu.read(cpu.registers.program_counter);
-    cpu.registers.program_counter++;
-    Byte high = cpu.read(cpu.registers.program_counter);
-    cpu.registers.program_counter++;
+    Byte low = cpu.read(cpu.fetchPC());
+    Byte high = cpu.read(cpu.fetchPC());
 
     Word address = high << 8 | low;
 
@@ -42,22 +40,25 @@ void SixteenBitLoadInstructionSet::ld_a16_sp(CPU& cpu) {
 }
 
 void SixteenBitLoadInstructionSet::ld_sp_n16(CPU& cpu) {
-    Byte low = cpu.read(cpu.registers.program_counter);
-    cpu.registers.program_counter++;
-    Byte high = cpu.read(cpu.registers.program_counter);
-    cpu.registers.program_counter++;
+    Byte low = cpu.read(cpu.fetchPC());
+    Byte high = cpu.read(cpu.fetchPC());
 
     cpu.registers.stack_pointer = high << 8 | low;
 }
 
-void SixteenBitLoadInstructionSet::pop_rr(CPU& cpu, Byte& high, Byte& low) {
-    low = cpu.read(cpu.registers.stack_pointer);
+void SixteenBitLoadInstructionSet::pop_rr(CPU& cpu, int pair) {
+    Byte low = cpu.read(cpu.registers.stack_pointer);
     cpu.registers.stack_pointer++;
-    high = cpu.read(cpu.registers.stack_pointer);
+    Byte high = cpu.read(cpu.registers.stack_pointer);
     cpu.registers.stack_pointer++;
+
+    cpu.setPair(pair, high << 8 | low);
 }
 
-void SixteenBitLoadInstructionSet::push_rr(CPU& cpu, Byte& high, Byte& low) {
+void SixteenBitLoadInstructionSet::push_rr(CPU& cpu, int pair) {
+    Byte low = cpu.getPair(pair) & 0xFF;
+    Byte high = cpu.getPair(pair) >> 8;
+    
     cpu.registers.stack_pointer--;
     cpu.write(cpu.registers.stack_pointer, high);
     cpu.registers.stack_pointer--;
@@ -65,18 +66,17 @@ void SixteenBitLoadInstructionSet::push_rr(CPU& cpu, Byte& high, Byte& low) {
 }
 
 void SixteenBitLoadInstructionSet::lp_hl_sp_e8(CPU& cpu) {
-    int e8 = (int) cpu.read(cpu.registers.program_counter);
-    cpu.registers.program_counter++;
+    int e8 = (int) cpu.read(cpu.fetchPC());
 
     Word result = cpu.registers.stack_pointer + e8;
-    cpu.registers.set_pair(result, cpu.registers.h_register, cpu.registers.l_register);
+    cpu.setPair(HL, result);
 
-    cpu.registers.set_bit(cpu.registers.flag_register, Z, false);
-    cpu.registers.set_bit(cpu.registers.flag_register, N, false);
-    cpu.registers.set_bit(cpu.registers.flag_register, H, result & 0x08);
-    cpu.registers.set_bit(cpu.registers.flag_register, C, result & 0x80);
+    cpu.setBit(cpu.registers.f, Z, false);
+    cpu.setBit(cpu.registers.f, N, false);
+    cpu.setBit(cpu.registers.f, H, result & 0x08);
+    cpu.setBit(cpu.registers.f, C, result & 0x80);
 }
 
 void SixteenBitLoadInstructionSet::lp_sp_hl(CPU& cpu) {
-    cpu.registers.stack_pointer = cpu.registers.get_pair(cpu.registers.h_register, cpu.registers.l_register);
+    cpu.registers.stack_pointer = cpu.getPair(HL);
 }
